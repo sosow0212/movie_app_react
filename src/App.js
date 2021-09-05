06f442f700b846c9d9ad843ff85bcbe0b736cdd7
sosow0212 <sosow0212@naver.com>
2021-09-05 16:15:44 +0900

    Study-PropTypes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food(props) {
-  console.log();
-  return (<h1>I like {props.fav} </h1>);
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating: 4.3
+  },
+  {
+    id: 2,
+    name: "bulgogi",
+    image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM.jpg",
+    rating: 3.4
+  },
+  {
+    id: 3,
+    name: "kimbap",
+    image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM.jpg",
+    rating: 5.0
+  },
+  {
+    id: 4,
+    name: "samgyetang",
+    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    rating: 4.5
+  }
+];
+
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h1>I like {name} </h1>
+      <h2>{rating} / 5.0 </h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
-      <Food fav="kimchi"/>
-      <Food fav="ramen"/>
-      <Food fav="kimbob"/>
-      <Food fav="water"/>
+      {foodILike.map(dish => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating} />
+      ))}
     </div>
   );
 }
